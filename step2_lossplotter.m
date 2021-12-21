9a49cfc713fb79cb96a26fe3923ebb48a5d5c34f
Arman Hassanniakalager <85748589+hkalager@users.noreply.github.com>
2021-12-21 18:44:36 +0000

    Add files via upload

--- step2_lossplotter.m
+++ step2_lossplotter.m
@@ -3,7 +3,15 @@ clc;
 tickerlistaaa={'SPY','QQQ','GLD','USO'};
 tickernames=tickerlistaaa;
 %tickernames={'S&P500','NASDAQ','Gold','Light Oil'};
-loss_b_range=[-2];
+loss_b_range=0;
+switch loss_b_range
+    case -2
+        loss_label='QLIKE';
+    case 0
+        loss_label='MSE';
+    otherwise
+        loss_label=['beta=',num2str(loss_b_range)];
+end
 Benchmark={'GARCH','GJR-GARCH','HAR'};
 
 try
@@ -24,9 +32,9 @@ for s=1:numel(bench_ind)
 end
 
 i_l=5;
-nbin=20;
+nbin=10;
 rng(0);
-figure,
+Perf_bar_matrix=[];
 for ta=1:numel(tickerlistaaa)
     flname=[tickerlistaaa{ta},'_Pool_M5_OOS_2014_2020.mat'];
     load(flname,'oosdate','oos_ser','TF1SMP','TF2SMP','tbl0');
@@ -37,45 +45,16 @@ for ta=1:numel(tickerlistaaa)
     poolset_ser=oos_ser_tested;
     target=tbl0{TF1SMP:end,'RVDaily'};
     modelscount=size(oos_ser,2);
-    iter=0;
-    for l=1:numel(loss_b_range)
-        iter=iter+1;
-        Perf(iter,:)=robust_loss_fn(poolset_ser,target,loss_b_range(l));
+
+    Perf(1,:)=robust_loss_fn(poolset_ser,target,loss_b_range);
         
-    end
     Perf=abs(Perf);
     Perf(Perf>5*mean(Perf(:,1)))=5*mean(Perf(:,1));
     Perf_bar=real(mean(Perf,1,'omitnan'));
-    
-    Up_lim=max(Perf_bar);
-    Low_lim=min(Perf_bar);
-    cdf=zeros(1,nbin);
-    supp_points=linspace(Low_lim,Up_lim,nbin);
-    for s=1:nbin
-        cdf(s)=sum(Perf_bar<=supp_points(s));
-    end
-    pdf=diff(cdf);
-    lim_pdf=max(pdf);
-    subplot(numel(tickerlistaaa)/2,2,ta);
-    highplotrange=prctile(Perf_bar,95);
-    lowplotrange=prctile(Perf_bar,5);
-    plotrng=(highplotrange-lowplotrange)/nbin;
-    ax1=histogram(Perf_bar,lowplotrange:plotrng:highplotrange,'DisplayStyle','bar','Normalization','probability');hold all;
-    %ax1=histogram(Perf_bar,20,'DisplayStyle','bar','Normalization','probability');hold all;
-    % GARCH
-    ax2=plot(Perf_bar(bench_ind(1)),0,'sk','LineWidth',.75);
-    % GJR-GARCH
-    ax3=plot(Perf_bar(bench_ind(2)),0,'dk','LineWidth',.75);
-    % HAR
-    ax4=plot(Perf_bar(bench_ind(3)),0,'*k','LineWidth',.75);
-    
-    grid('on')
-    title(tickernames{ta});
-    xlabel('Loss');
-    ylabel('Density');
-    legend([ax2,ax3,ax4],'GARCH','GJR-GARCH','HAR','Location','best');
-    legend('boxoff');
-    hold off;
-    set(gcf,'WindowState','maximized');
+    Perf_bar_matrix(:,ta)=Perf_bar';
+   
 end
 
+boxplot(Perf_bar_matrix,'PlotStyle','traditional','Symbol','bo','Colors','k','Widths',.75,'Jitter',.15,'Labels',tickerlistaaa);title(loss_label)
+
+%set(gcf,'WindowState','maximized');
